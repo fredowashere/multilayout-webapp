da4a766d2f2f2cc20a96f259e481110bd34cf5bb
Federico Trotta <117639221+fredowashere@users.noreply.github.com>
2023-03-16 10:09:01 +0100

    Autoscroll to top on route change in dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, Subject, takeUntil, tap } from 'rxjs';
 import { SidebarService } from '../services/sidebar.service';
 
 @Component({
@@ -8,7 +10,26 @@ import { SidebarService } from '../services/sidebar.service';
 })
 export class DashboardComponent {
 
+  destroy$ = new Subject<void>();
+
   constructor(
-    public sidebarService: SidebarService
+    public sidebarService: SidebarService,
+    private router: Router
   ) { }
+
+  ngOnInit() {
+
+    // Autoscroll to top on route change
+    this.router.events
+      .pipe(
+        takeUntil(this.destroy$),
+        filter(event => event instanceof NavigationEnd),
+        tap(() => document.querySelector('.sb-layout__main')?.scrollTop)
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+  }
 }
