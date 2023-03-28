94cf97fbc91e0ba1577179eaf13e4e37a323da06
Federico Trotta <117639221+fredowashere@users.noreply.github.com>
2023-03-28 11:01:16 +0200

    Fix login procedure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggedInGuard } from './guards/logged-in.guard';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
       import('./dashboard/dashboard.module').then(m => m.DashboardModule),
-    canActivate: ["loggedInOnlyGuard"]
+    canActivate: [ LoggedInGuard, "attivitaUserGuard" ]
   },
   {
     path: 'login',
