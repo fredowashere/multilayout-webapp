50fe38e752cf241c8b844a19962fc862a761ce28
Federico Trotta <117639221+fredowashere@users.noreply.github.com>
2023-05-26 20:03:23 +0200

    Fix enforceMinMax input util

--- src/app/utils/input.ts
+++ src/app/utils/input.ts
@@ -1,7 +1,14 @@
-export function enforceMinMax(el: HTMLInputElement) {
-    if (el.value != "")
-      if (+el.value < +el.min)
-        el.value = el.min;
-      if (+el.value > +el.max)
-        el.value = el.max;
+export function enforceMinMax(el: HTMLInputElement, minOverride?: number, maxOverride?: number) {
+
+  const min = (minOverride !== undefined) && minOverride || parseInt(el.min);
+  const max = (maxOverride !== undefined) && maxOverride || parseInt(el.max);
+
+  if (el.value !== "") {
+
+    if (parseInt(el.value) < min)
+      setTimeout(() => el.value = min.toString(), 0);
+
+    if (parseInt(el.value) > max)
+      setTimeout(() => el.value = max.toString(), 0);
+  }
 }
